ed5260ac1efa5fd44afda0415ab2fea20990ab95
Jens <jensbehmenburg@gmail.com>
2021-09-19 23:12:05 +0200

    feat: initial commit

---
+++ src/main/java/com/restserveraccord/RestServerAccordApplication.java
@@ -0,0 +1,13 @@
+package com.restserveraccord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerAccordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServerAccordApplication.class, args);
+	}
+
+}
